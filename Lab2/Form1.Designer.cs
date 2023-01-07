307eca111e40d8e554a4e683fa0909e0b65dfd52
Oleksii-Bychkov <oleksii.bychkov@lll.kpi.ua>
2023-01-07 17:53:24 +0200

    Lab5

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace cryptoXorRtf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.keyBox = new System.Windows.Forms.TextBox();
+            this.xorButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cBox = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(36, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "P = ";
+            // 
+            // pBox
+            // 
+            this.pBox.Location = new System.Drawing.Point(65, 46);
+            this.pBox.Name = "pBox";
+            this.pBox.Size = new System.Drawing.Size(118, 22);
+            this.pBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(26, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Key=";
+            // 
+            // keyBox
+            // 
+            this.keyBox.Location = new System.Drawing.Point(65, 71);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(118, 22);
+            this.keyBox.TabIndex = 3;
+            // 
+            // xorButton
+            // 
+            this.xorButton.Location = new System.Drawing.Point(202, 58);
+            this.xorButton.Name = "xorButton";
+            this.xorButton.Size = new System.Drawing.Size(65, 26);
+            this.xorButton.TabIndex = 4;
+            this.xorButton.Text = "XOR";
+            this.xorButton.UseMnemonic = false;
+            this.xorButton.UseVisualStyleBackColor = true;
+            this.xorButton.Click += new System.EventHandler(this.OnXorClick);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(36, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 30);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "C=";
+            // 
+            // cBox
+            // 
+            this.cBox.Location = new System.Drawing.Point(65, 99);
+            this.cBox.Name = "cBox";
+            this.cBox.ReadOnly = true;
+            this.cBox.Size = new System.Drawing.Size(118, 22);
+            this.cBox.TabIndex = 6;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(65, 132);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(117, 34);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "Прибрати";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.OnClearClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 211);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.cBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.xorButton);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button clearButton;
+
+
+        private System.Windows.Forms.TextBox keyBox;
+
+        private System.Windows.Forms.TextBox pBox;
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox cBox;
+
+        private System.Windows.Forms.Button xorButton;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+
+        #endregion
+    }
+}
---
+++ Lab5/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cryptoRtfHash
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private byte[] myEncoding(byte[] inArr)
+        {
+            byte[] result = { 0 };
+
+            if (rB_CRC32.Checked == true)
+            {
+                uint a = Crc.Crc32(inArr);
+                return BitConverter.GetBytes(a);
+            }
+
+            if (rB_HAVAL.Checked == true)
+            {
+                HashAlgorithm sha = KeyedHashAlgorithm.Create();
+                return sha.ComputeHash(inArr);
+            }
+
+            if (rB_RIPEMD160.Checked == true)
+            {
+                HashAlgorithm sha = RIPEMD160.Create();
+                return sha.ComputeHash(inArr);
+            }
+            if (rB_MD5.Checked == true)
+            {
+                HashAlgorithm sha = MD5.Create();
+                return sha.ComputeHash(inArr);
+            }
+            if (rB_SHA1.Checked == true)
+            {
+                HashAlgorithm sha = SHA1.Create();
+                return sha.ComputeHash(inArr);
+            }
+            if (rB_SHA256.Checked == true)
+            {
+                HashAlgorithm sha = SHA256.Create();
+                return sha.ComputeHash(inArr);
+            }
+            if (rB_SHA384.Checked == true)
+            {
+                HashAlgorithm sha = SHA384.Create();
+                return sha.ComputeHash(inArr);
+            }
+            if (rB_SHA512.Checked == true)
+            {
+                HashAlgorithm sha = SHA512.Create();
+                return sha.ComputeHash(inArr);
+            }
+
+            return result;
+        }
+
+        private void bInFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Шлях до файлу
+                    string fileINpath = openFileDialog.FileName;
+                    tInFilePath.Text = fileINpath;
+                    var fileInfo = new FileInfo(fileINpath);
+                    inFileLabel.Text = "Вхідного файлу: " + fileInfo.Length;
+                }
+            }
+        }
+
+        private void bOutFile_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+            
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Шлях до файлу
+                string fileOutpath = saveFileDialog.FileName;
+                tOutFilePath.Text = fileOutpath;
+            }
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(tInFilePath.Text)) return;
+            byte[] inArr = File.ReadAllBytes(tInFilePath.Text);
+            var stopwatch = new Stopwatch();
+            // Begin timing.
+            stopwatch.Start();
+            var outArr = myEncoding(inArr);
+            stopwatch.Stop();
+            label_time.Text = stopwatch.Elapsed.ToString(@"mm\:ss\.fff");
+            // Write result.
+            File.WriteAllBytes(tOutFilePath.Text, outArr); // створюємо новий файл і записуємо у нього 
+            string result = BitConverter.ToString(outArr).Replace("-", "");
+            var fileInfo = new FileInfo(tOutFilePath.Text);
+            outFileLabel.Text = "Вихідного файлу: " + fileInfo.Length;
+            MessageBox.Show("У файлі записане число (контрольна сума вхідного файлу):\n" + result, "Результат", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void bClean_Click(object sender, EventArgs e)
+        {
+            tInFilePath.Text = "";
+            tOutFilePath.Text = "";
+            inFileLabel.Text = "Вхідного файлу: ???";
+            outFileLabel.Text = "Вихідного файлу: ???";
+            rB_CRC32.Checked = false;
+            rB_HAVAL.Checked = false;
+            rB_RIPEMD160.Checked = false;
+            rB_MD5.Checked = false;
+            rB_SHA1.Checked = false;
+            rB_SHA256.Checked = false;
+            rB_SHA384.Checked = false;
+            rB_SHA512.Checked = false;
+        }
+
+        private void authorLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Oleksiy Bychkov");
+        }
+
+        private void descriptionLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Example of hashing algorithms");
+        }
+    }
+}
